b373f56d1474383d80d8d890b57791edce028519
Philipp-Eisermann <41010350+Philipp-Eisermann@users.noreply.github.com>
2019-05-10 08:57:04 +0200

    First

---
+++ client1.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+"""Script for Tkinter GUI chat client."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+import time
+
+#- - - - - - - - - - - - - - - - R S A - - - - - - - - - - - - - - - -
+
+def gcd(a, h):
+    #trouver le pgcd de a et h
+    while (1):
+        temp = a % h
+        if (temp == 0):
+            return h
+            break
+        a = h
+        h = temp
+
+def publickey(p, q):
+    # Premiere partie de la cle publique
+    n = p*q
+
+    # Deuxieme partie de la cle publique
+    # e pour encrypt
+    phi = (p-1)*(q-1)
+    e = 2
+    while (e < phi):
+        # Trouver e tel que e coprime a phi et e<phi
+        if (gcd(e, phi) == 1):
+            break
+        else:
+            e += 1
+    return [n, e]
+
+def privatekey(p, q, e):
+    #regenerer phi
+    phi = (p-1)*(q-1)
+    # Generer clee privee d comme decrypt
+    # d = (1 + k*phi) / e pour k une variable int constante, (ex. k=2)
+    k = 2
+    d = ( 1 + (k*phi) ) / e
+    return d
+
+def encrypt(msg, n, e):
+    # Trouver c, le message crypte
+    # c = (msg ^ e) % n
+    c = pow(msg, e) % n
+    return c
+
+def decrypt(c, d, n):
+    # msg = (c^d) % n
+    m = pow(c, d) % n
+    return m
+
+#- - - - - - - - - - - - - - - - SOCKET - - - - - - - - - - - - - - - -
+
+def receive():
+    """Handles receiving of messages."""
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+
+        except OSError:  # Possibly client has left the chat.
+            break
+
+
+def send(event=None):  # event is passed by binders.
+    """Handles sending of messages."""
+    msg = my_msg.get()
+
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "/quit":
+        client_socket.close()
+        top.quit()
+
+
+def on_closing(event=None):
+    """This function is to be called when the window is closed."""
+    my_msg.set("/quit")
+    send()
+
+def setup_rsa(n, e):
+    # envoyer au serveur la clef publique et recevoir la clef publique de l'autre client
+    client_socket.send(bytes(str(n), "utf8"))
+    time.sleep(1)
+    client_socket.send(bytes(str(e), "utf8"))
+    time.sleep(1)
+    n2 = client_socket.recv(BUFSIZ).decode("utf8")
+    time.sleep(1)
+    e2 = client_socket.recv(BUFSIZ).decode("utf8")
+    return [n2, e2]
+
+
+#- - - - - - - - - - - - - - - - TKINTER - - - - - - - - - - - - - - - -
+top = tkinter.Tk()
+top.title("Chatter")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg.set("Type your messages here.")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+#- - - - - - - - - - - - - - - - M A I N - - - - - - - - - - - - - - - -
+
+input_p = 164051
+input_q = 164057
+
+gen_n, gen_e = publickey(input_p, input_q)
+gen_d = privatekey(input_p, input_q, gen_e)
+
+HOST = input('Enter host: ')
+PORT = input('Enter port: ')
+if not PORT:
+    PORT = 33000
+else:
+    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+#client2_n, client_e = setup_rsa(gen_n, gen_e)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()  # Starts GUI execution.
---
+++ server.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""Server for multithreaded (asynchronous) chat application."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import time
+
+
+
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client, client_address)).start()
+
+
+def handle_client(client, client_address):  # Takes client socket as argument.
+    """Handles a single client connection."""
+
+    client.send(bytes("Hi, what's your name?", "utf8"))
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type /quit to exit.' % name
+
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+
+        if msg != bytes("/quit", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("/quit", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+def rsa():
+    return 0
+    #client2_n = client.recv(BUFSIZ).decode("utf8")
+    #print(str(client1_n))
+    #client2_e = client.recv(BUFSIZ).decode("utf8")
+    #print(str(client1_e))
+    #client.send(bytes(client1_n, "utf8"))
+    #time.sleep(1)
+    #client.send(bytes(client1_e, "utf8"))
+
+clients = {}
+addresses = {}
+
+
+
+HOST = 'localhost'
+PORT = 5566
+RSAPORT = 8080
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+RSAADDR = (HOST, RSAPORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+RSA = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+RSA.bind(RSAADDR)
+
+
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    RSA.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
+    RSA.close()
